400fad94020aa64321935781191ba2889c5c7315
AB <btaman4@gmail.om>
2021-02-10 02:33:47 -0500

    Adding javascript files exported from github.com/jannunzi, with some minor changes

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,7 +4,7 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/bhaumik/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001877232/users';
     var self = this;
 
 
