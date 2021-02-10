7b31814797668a7802332073a9fe8551c75ec89c
AB <btaman4@gmail.om>
2021-02-10 18:24:50 -0500

    remove th title from column 4

--- src/main/webapp/user-admin/user-admin.component.client.js
+++ src/main/webapp/user-admin/user-admin.component.client.js
@@ -6,12 +6,13 @@ let $roleFld;
 let $searchBtn;
 let $createBtn;
 let $updateBtn;
-let tBody;
+let adminBody;
 let users[] = 0
 
 
 // starting the function
 // call to service client "user.service.client.js" to connect to db and initiate a new session
-let userService = new AdminUserServiceClient();
+
+let userService = new AdminUserServiceClient()
 
 
