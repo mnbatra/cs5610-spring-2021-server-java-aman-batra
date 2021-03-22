3277fa8ef62b409bbbca8da6a8fa4a3187e4e133
Aman Batra <btaman4@gmail.om>
2021-03-21 21:56:02 -0400

    added all vars to model

--- src/main/java/com/example/wbdvsp2101amanbatraserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp2101amanbatraserverjava/models/Widget.java
@@ -1,14 +1,50 @@
 package com.example.wbdvsp2101amanbatraserverjava.models;
+
 public class Widget {
     private Long id;
 
-    private String topicIc;
+    private String topicId;
+
+    private String name;
 
     private String type;
 
-    private Integer size;
-    
+    private Integer widgetOrder;
+
     private String text;
+
+    private String src;
+
+    private Integer size;
+
+    private Integer width;
+
+    private Integer height;
+
+    private String cssClass;
+
+    private String style;
+
+    private String value;
+
+    public Widget(Long id, String topicId, String name, String type, Integer widgetOrder, String text, String src, Integer size, Integer width, Integer height, String cssClass, String style, String value) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.widgetOrder = widgetOrder;
+        this.topicId = topicId;
+        this.text = text;
+        this.src = src;
+        this.size = size;
+        this.width = width;
+        this.height = height;
+        this.cssClass = cssClass;
+        this.style = style;
+        this.value = value;
+    }
+
+    public Widget() {
+    }
 
     public Long getId() {
         return id;
@@ -18,20 +54,20 @@ public class Widget {
         this.id = id;
     }
 
-    public String getTopicIc() {
-        return topicIc;
+    public String getTopicId() {
+        return topicId;
     }
 
-    public void setTopicIc(String topicIc) {
-        this.topicIc = topicIc;
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
     }
 
-    public void setSize(Integer size) {
-        this.size = size;
+    public String getName() {
+        return name;
     }
 
-    public String getText() {
-        return text;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getType() {
@@ -42,23 +78,76 @@ public class Widget {
         this.type = type;
     }
 
-    public Integer getSize() {
-        return size;
+    public Integer getWidgetOrder() {
+        return widgetOrder;
+    }
+
+    public void setWidgetOrder(Integer widgetOrder) {
+        this.widgetOrder = widgetOrder;
+    }
+
+    public String getText() {
+        return text;
     }
 
     public void setText(String text) {
         this.text = text;
     }
 
-    public Widget(Long id, String topicIc, String type, Integer size, String text) {
-        this.id = id;
-        this.topicIc = topicIc;
-        this.type = type;
-        this.size = size;
-        this.text = text;
+    public String getSrc() {
+        return src;
     }
 
-    public Widget() {
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
     }
 }
-
